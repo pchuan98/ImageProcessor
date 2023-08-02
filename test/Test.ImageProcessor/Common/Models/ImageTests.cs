d67e6fdfce003b1e4abe25c8e33859a508b2d737
chuan <1114003209@qq.com>
2023-08-02 18:11:30 +0800

    add test app

---
+++ test/Test.ImageProcessor/Common/Models/ImageTests.cs
@@ -0,0 +1,32 @@
+﻿using ImageProcessor.Common.Models;
+using NUnit.Framework;
+
+namespace Test.ImageProcessor.Common.Models
+{
+    [TestFixture]
+    public class ImageTests
+    {
+        public const string Path1 = @"F:\Company\src\Simscop.Visual\test\1.jpg";
+
+        public const string Path2 = @"F:\Company\src\Simscop.Visual\test\d16.jpg";
+
+        public const string Path3 = @"F:\Company\src\Simscop.Visual\test\d48.png";
+
+        public const string Path4 = @"F:\Company\src\Simscop.Visual\test\hela .png";
+
+
+        public ImageTests()
+        {
+
+        }
+
+        [Test]
+        public void Dispose_StateUnderTest_ExpectedBehavior()
+        {
+            var image = new Image();
+            image.Dispose();
+
+            Assert.Fail();
+        }
+    }
+}
---
+++ test/Test.ImageProcessor/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Test.ImageProcessor
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
